44fae1fd649c1ea8ba9350315bdea20f03c4dc7a
Aviap09 <luispaiva@ipvc.pt>
2025-07-07 22:44:20 +0100

    Slides Activities

--- app/src/main/java/com/example/studyshare/Activities/SlidesActivity.kt
+++ app/src/main/java/com/example/studyshare/Activities/SlidesActivity.kt
@@ -18,40 +18,22 @@ class SlidesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+        if (prefs.getBoolean("onboardingFinished", false)) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
         setContentView(R.layout.activity_slides)
 
         viewPager = findViewById(R.id.viewPager)
         btnNext = findViewById(R.id.btnNext)
 
         val adapter = SlideAdapter(this)
-        adapter.addFragment(
-            SlideFragment.newInstance(
-                R.drawable.logo,
-                "Bem-vindo à StudyShare!",
-                "Partilhe e Estude materiais educativos com outros estudantes!"
-            )
-        )
-        adapter.addFragment(
-            SlideFragment.newInstance(
-                R.drawable.logo2,
-                "Diversidade de Perspetivas",
-                "Com perspetivas diferentes de vários estudantes, ampliamos a visão sobre o estudo que estamos a realizar!"
-            )
-        )
-        adapter.addFragment(
-            SlideFragment.newInstance(
-                R.drawable.logo3,
-                "Acesso a Recursos Complementares",
-                "Com a partilha de recursos entre vários estudantes, temos um maior escopo de material ao qual podemos utilizar o qual não estaria disponível individualmente!"
-            )
-        )
-        adapter.addFragment(
-            SlideFragment.newInstance(
-                R.drawable.logo4,
-                "Aumento da produtividade",
-                "Ao trabalhar em conjunto, podemos realizar trabalhos e projetos mais rapidamente devido à cooperação e divisão de tarefas!"
-            )
-        )
+        adapter.addFragment(SlideFragment.newInstance(R.drawable.logo, "Bem-vindo à StudyShare!", "Partilhe e Estude materiais educativos com outros estudantes!"))
+        adapter.addFragment(SlideFragment.newInstance(R.drawable.logo2, "Diversidade de Perspetivas", "Com perspetivas diferentes de vários estudantes, ampliamos a visão sobre o estudo que estamos a realizar!"))
+        adapter.addFragment(SlideFragment.newInstance(R.drawable.logo3, "Acesso a Recursos Complemetares", "Com a partilha de recursos entre vários estudantes, temos um maior escopo de material ao qual podemos utilizar o qual não estaria disponível individualmente!"))
+        adapter.addFragment(SlideFragment.newInstance(R.drawable.logo4, "Aumento da produtividade", "Ao trabalhar em conjunto, podemos realizar trabalhos e projetos mais rapidamente devido à cooperação e divisão de tarefas!"))
 
         viewPager.adapter = adapter
 
@@ -73,6 +55,7 @@ class SlidesActivity : AppCompatActivity() {
             if (viewPager.currentItem < adapter.itemCount - 1) {
                 viewPager.currentItem += 1
             } else {
+                prefs.edit().putBoolean("onboardingFinished", true).apply()
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }
